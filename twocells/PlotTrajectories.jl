23c966cd1fae426ce97bafc46fc81352b8f4ee33
Dominic Skinner <dominic@Dominics-MacBook-Pro-4.local>
2025-02-25 19:53:36 -0500

    updated minimally PlotTrajectories and plot_ctmc function

--- twocells/PlotTrajectories.jl
+++ twocells/PlotTrajectories.jl
@@ -1,9 +1,31 @@
-# assume we can load Q_opt_absorbing from somewhere
+using Plots
+using CellularDecisions
 
+function average_trajectory(path_array, time_points)
+    ua_avg = zeros(length(time_points))
+    ub_avg = zeros(length(time_points))
+    ua_std = zeros(length(time_points))
+    ub_std = zeros(length(time_points))
+
+    for (i,t) in enumerate(time_points)
+        ua_pts = [path.u1[find_state_at_time(path,t)] for path in path_array]
+        ub_pts = [path.u2[find_state_at_time(path,t)] for path in path_array]
+
+        ua_avg[i] = mean(ua_pts)
+        ub_avg[i] = mean(ub_pts)
+
+        ua_std[i] = std(ua_pts)
+        ub_std[i] = std(ub_pts)
+    end
+    return ua_avg, ub_avg, ua_std, ub_std
+end
+
+
+
+# This script is half complete, I'm assuming we can load Q_opt_absorbing from somewhere
 S_arr  = [CellularDecisions.simulate_ctmc(Q_opt_absorbing, initial_state, T,N) for i = 1:4000]
 CellularDecisions.save(S_arr,"tmp.h5")
 S2_arr = CellularDecisions.load("tmp.h5")
-
 
 targetstates_good=[target_state+1 for target_state ∈ TG];  # Good target states
 targetstates_bad=[target_state+1 for target_state ∈ TB];   # Bad target states
@@ -18,38 +40,32 @@ failed_trajectories = unpack.(S_arr[terminal_classes .== -1])
 success_trajectories = unpack.(S_arr[terminal_classes .== 1])
 
 t_plot = 0:0.5:50
-ua_avg_fail = zeros(length(t_plot))
-ub_avg_fail = zeros(length(t_plot))
-ua_avg_succ = zeros(length(t_plot))
-ub_avg_succ = zeros(length(t_plot))
 
-ua_std_fail = zeros(length(t_plot))
-ub_std_fail = zeros(length(t_plot))
-ua_std_succ = zeros(length(t_plot))
-ub_std_succ = zeros(length(t_plot))
+ua_avg_fail, ub_avg_fail, ua_std_fail, ub_std_fail = average_trajectory(failed_trajectories, t_plot)
+ua_avg_succ, ub_avg_succ, ua_std_succ, ub_std_succ = average_trajectory(success_trajectories, t_plot)
 
-for j = 1:length(t_plot)
-    ua_pts_fail = [f.u1[find_state_at_time(f,t_plot[j])]/length(failed_trajectories) for f in failed_trajectories]
-    ub_pts_fail = [f.u2[find_state_at_time(f,t_plot[j])]/length(failed_trajectories) for f in failed_trajectories]
-    ua_pts_succ = [f.u1[find_state_at_time(f,t_plot[j])]/length(success_trajectories) for f in success_trajectories]
-    ub_pts_succ = [f.u2[find_state_at_time(f,t_plot[j])]/length(success_trajectories) for f in success_trajectories]
-        
-    ua_avg_fail[j] = mean(ua_pts_fail) 
-    ub_avg_fail[j] = mean(ub_pts_fail)
-    ua_avg_succ[j] = mean(ua_pts_succ)
-    ub_avg_succ[j] = mean(ub_pts_succ)
-
-    ua_std_fail[j] = std(ua_pts_fail)
-    ub_std_fail[j] = std(ub_pts_fail)
-    ua_std_succ[j] = std(ua_pts_succ)
-    ub_std_succ[j] = std(ub_pts_succ)
-end
-
-using Plots
+# plot the average
 p1 = plot(t_plot,ua_avg_fail,ribbon = ua_std_fail,label="ua",xlabel="Time",ylabel="Probability",title="Unsuccesful trajectories")
 plot!(p1,t_plot,ub_avg_fail,ribbon = ub_std_fail,label="ub")
+
 
 p2 = plot(t_plot,ua_avg_succ,ribbon = ua_std_succ,label="ua",xlabel="Time",ylabel="Probability",title="Succesful trajectories")
 plot!(p2, t_plot,ub_avg_succ,ribbon = ub_std_succ,label="ub")
 
 plot(p1,p2,layout=(2,1))
+savefig("plots/Average_traj_q_absorb.pdf")
+
+# look at some individual trajectories
+p3 = plot()
+for i = 1:50
+    plot_ctmc!(p3,failed_trajectories[i].times, failed_trajectories[i].u1, failed_trajectories[i].final_time,c=:blue,linewidth=0.2)
+    plot_ctmc!(p3,failed_trajectories[i].times, failed_trajectories[i].u2, failed_trajectories[i].final_time,c=:red,linewidth=0.2)
+end
+
+p4 = plot()
+for i = 1:50
+    plot_ctmc!(p4,success_trajectories[i].times, success_trajectories[i].u1, success_trajectories[i].final_time,c=:blue,linewidth=0.2)
+    plot_ctmc!(p4,success_trajectories[i].times, success_trajectories[i].u2, success_trajectories[i].final_time,c=:red,linewidth=0.2)
+end
+
+plot(p3,p4,layout=(2,1))
--- utils/ctmc_vis.jl
+++ utils/ctmc_vis.jl
@@ -10,20 +10,18 @@ function plot_ctmc(S::StochasticPath)
         ylim=(minimum(S.states)-0.5, maximum(S.states)+0.5),
         grid=:both,
         size=(1500, 1000),left_margin=10Plots.mm, right_margin=10Plots.mm,bottom_margin=10Plots.mm)
-    plot_ctmc!(p,S)
+    plot_ctmc!(p,S.times,S.states,S.final_time)
     
     return p
 end
 
-function plot_ctmc!(p,S::StochasticPath;c=:gray)
-    times = S.times
-    states = S.states
-    final_time = S.final_time
+function plot_ctmc!(p,times, states, final_time;c=:gray,linewidth=2)
+    # Core plotting functionality for a set of discrete jumps between "states" and "times"
     for i in 1:(length(times)-1)
-        plot!(p,[times[i], times[i+1]], [states[i], states[i]], linewidth=2, color=c, label=false)
-        plot!([times[i+1], times[i+1]], [states[i], states[i+1]], linestyle=:dash, color=c, label=false)
+        plot!(p,[times[i], times[i+1]], [states[i], states[i]], linewidth=linewidth, color=c, label=false)
+        plot!([times[i+1], times[i+1]], [states[i], states[i+1]], linewidth=linewidth, color=c, label=false)
     end
-    plot!(p,[times[end], final_time], [states[end], states[end]], linewidth=2, color=c, label=false)
+    plot!(p,[times[end], final_time], [states[end], states[end]], linewidth=linewidth, color=c, label=false)
 end
 
 function plot_Q_with_colored_yticks(Q::Matrix, N::Int, special_ticks::Vector{Int}, filename, λ::Float64=missing; save_plots::Bool=true)
